acc141cc61201b6921e864339c61dad8e8d92add
Patryk <patryk.d.janus@gmail.com>
2024-10-23 14:26:20 +0200

    implement login end point plus small refactor

--- routes/auth.ts
+++ routes/auth.ts
@@ -6,29 +6,48 @@ import bcrypt from 'bcrypt'
 const router = express.Router()
 const prisma = new PrismaClient()
 
-router.post('/log-in', (req, res) => {
-	res.status(200).send('Logged in successfully')
+router.post('/login', async (req, res) => {
+	try {
+		const { email, password } = req.body
+
+		const user = await prisma.user.findFirstOrThrow({
+			where: {
+				email,
+			},
+		})
+
+		const isMatch = await bcrypt.compare(password, user.password)
+
+		if (isMatch) {
+			return res.status(200).json({ message: 'Login successful' })
+		} else {
+			return res.status(400).json({ message: 'Invalid credentials' })
+		}
+	} catch (err) {
+		return res.status(400).json({ message: 'Invalid credentials' })
+	}
 })
 
 router.post('/register', async (req, res) => {
-	const { email, password } = req.body
-
 	try {
+		const { email, password } = req.body
 		if (isValidEmail(email) && isValidPassword(password)) {
 			const hashedPassword = await bcrypt.hash(password, 10)
 
 			await prisma.user.create({
 				data: { email, password: hashedPassword },
 			})
-			res.status(201).json({ message: 'User registered successfully' })
+			return res.status(201).json({ message: 'User registered successfully' })
 		} else {
-			res.status(400).json({
+			return res.status(400).json({
 				error:
 					'Invalid credentials: Password or email does not meet requirements.',
 			})
 		}
 	} catch (error) {
-		res.status(500).json({ error: 'Something went wrong when creating a user' })
+		return res
+			.status(500)
+			.json({ error: 'Something went wrong when creating a user' })
 	}
 })
 
