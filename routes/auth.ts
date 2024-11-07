94e5336bedcf3d9a200df2e063e140be111cdb09
Patryk <patryk.d.janus@gmail.com>
2024-11-07 13:45:42 +0100

    formatting code & added email taken and no account found with this email error checks

--- routes/auth.ts
+++ routes/auth.ts
@@ -10,11 +10,17 @@ router.post('/login', async (req, res) => {
 	try {
 		const { email, password } = req.body
 
-		const user = await prisma.user.findFirstOrThrow({
+		const user = await prisma.user.findFirst({
 			where: {
 				email,
 			},
 		})
+
+		if (!user) {
+			return res
+				.status(404)
+				.json({ error: 'No account found with the provided email address' })
+		}
 
 		const isMatch = await bcrypt.compare(password, user.password)
 
@@ -33,19 +39,31 @@ router.post('/login', async (req, res) => {
 router.post('/register', async (req, res) => {
 	try {
 		const { email, password, name } = req.body
-		if (isValidEmail(email) && isValidPassword(password) && name !== '') {
-			const hashedPassword = await bcrypt.hash(password, 10)
-			await prisma.user.create({
-				data: { email, password: hashedPassword, name },
-			})
-			return res.status(201).json({ message: 'User registered successfully' })
-		} else {
+
+		if (!isValidEmail(email) || !isValidPassword(password) || !name) {
 			return res.status(400).json({
 				error:
-					'Invalid credentials: Password, email or name does not meet requirements.',
+					'Invalid credentials: Password, email, or name does not meet requirements.',
 			})
 		}
+
+		const isEmailTaken = await prisma.user.findFirst({ where: { email } })
+		if (isEmailTaken) {
+			return res.status(409).json({
+				error:
+					"The email you've entered is already taken. Please enter a new email address or try logging in.",
+			})
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10)
+
+		await prisma.user.create({
+			data: { email, password: hashedPassword, name },
+		})
+
+		return res.status(201).json({ message: 'User registered successfully' })
 	} catch (error) {
+		console.error(error)
 		return res
 			.status(500)
 			.json({ error: 'Something went wrong when creating a user on our side' })
